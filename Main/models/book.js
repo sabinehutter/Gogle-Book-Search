f09935105413aeaab0f1c390fc61e46e441e7f60
Sabine-Hutter <Sabine.Hutter@ibm.com>
2021-01-06 01:23:24 -0500

    fixing seed file

--- Main/models/book.js
+++ Main/models/book.js
@@ -7,8 +7,8 @@ const bookSchema = new Schema({
   authors: { type: [String], required: true },
   link: { type: String, required: true },
   description: { type: String, required: true },
-  image: { type: String, required: true },
-  googleId: { type: String, required: true, unique: true }
+  image: { type: String, required: true }
+  // googleId: { type: String, required: true, unique: true }
 });
 
 const Book = mongoose.model("Book", bookSchema);
